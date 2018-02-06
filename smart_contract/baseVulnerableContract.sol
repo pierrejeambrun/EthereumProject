b41ca44f2e5b7380fa1a3adeb89003e772adffe1
Kytyan <pierre.delannoy@supelec.fr>
2018-02-06 10:50:19 +0100

    Update baseVulnerableContract.sol
    DelegateCall to a library working

--- smart_contract/baseVulnerableContract.sol
+++ smart_contract/baseVulnerableContract.sol
@@ -6,11 +6,28 @@ pragma solidity ^0.4.0;
  To use delegateCall, the library address must be hardcoded
 ***********/
 
+contract TestLibrary {
+    event reached();
+    
+    function canIReachThis() public {
+       reached();
+       //return true;
+    }
+}
+
 contract vulnerableContract {
-    address constant _library = 0xcafecafecafecafecafecafecafecafecafecafe;
-  
+    //Ici mettre l'adresse de la librairie minée au préalable
+    address constant _library = 0x692a70d2e424a56d2c6c27aa97d1a86395877b3a;
+    
+    event yesItWasCalled();
+
+
+    function getAddressLib() pure public returns (address) {
+        return _library;    
+    }
+    
     // gets called when no other function matches
-    function() payable {
+    function() payable public {
         //If I get sent some cash (wallet logic)
         if (msg.value > 0) {
             //Do something
@@ -19,7 +36,10 @@ contract vulnerableContract {
         //Error here in delegateCall, still trying to find what it is
         else if (msg.data.length > 0) {
             //Delegate call to some library
-            _library.delegatecall(msg.data);
+            yesItWasCalled();
+            //Below commented is call to canIReachThis method of the librairy which fires an event
+            _library.delegatecall(keccak256("canIReachThis()"));
+            //_library.delegatecall(msg.data);
         }
     } 
 }
