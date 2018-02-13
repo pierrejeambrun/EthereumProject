5b18e846565c5ee66cac86d40d6b71e2cb6e00b9
Kytyan <pierre.delannoy@supelec.fr>
2018-02-13 09:27:09 +0100

    Update baseVulnerableContract.sol
    Vuln UP !

--- smart_contract/baseVulnerableContract.sol
+++ smart_contract/baseVulnerableContract.sol
@@ -8,14 +8,14 @@ pragma solidity ^0.4.0;
 
 contract TestLibrary {
     
-    address _owner;
+    address public _owner;
     
     event reached();
     event reachedInit(address);
     
     function init(address _from) public {
         reachedInit(_from);
-        //this._owner = _from;
+        _owner = _from;
     }
     
     function canIReachThis() public {
@@ -25,34 +25,34 @@ contract TestLibrary {
 }
 
 contract VulnerableContract {
+    
     event tryInit(address);
     //Declaration of the event that will be used.
     event yesItWasCalled();
-
+    
     //Ici mettre l'adresse de la librairie minée au préalable
-    address constant _library = 0x0dcd2f752394c41875e259e00bb44fd505297caf;
+    address constant _library = 0x0fdf4894a3b7c5a101686829063be52ad45bcfb7;
     
-    address _owner ;
+    address public _owner;
     
-    function formatFName(string fName) private returns (bytes32) {
-        bytes32 result = keccak256(fName);
+    function formatFSig(string fSig) private returns (bytes4) {
+        bytes32 result = keccak256(fSig);
         //Call byte4 pour tronquer le hash, sinon les params sont modifiés
         return bytes4(result);
     }
     
+    /***
+     * Format address, useless for now
     function formatAddress(address fAddr) private returns (bytes32) {
         bytes32 result = keccak256(fAddr);
         return result;
     }
+    ***/
     
     //Constructor of the wallet. Delegates the initialisation of _owner to the library
     function VulnerableContract() public {
         tryInit(msg.sender);
-        _library.delegatecall(formatFName("init(address)"), msg.sender);
-    }
-    
-    function getOwner() view public returns (address) {
-        return _owner;
+        _library.delegatecall(formatFSig("init(address)"), msg.sender);
     }
     
     function getAddressLib() pure public returns (address) {
@@ -69,12 +69,12 @@ contract VulnerableContract {
         //Error here in delegateCall, still trying to find what it is
         else if (msg.data.length > 0) {
             //Delegate call to some library
-            yesItWasCalled();
+            //yesItWasCalled();
             //Below commented is call to canIReachThis method of the librairy which fires an event and later will do the logic.
             //_library.delegatecall(keccak256("canIReachThis()"));
             _library.delegatecall(msg.data);
         }
-    } 
+    }
 }
 
 /**********
@@ -83,9 +83,18 @@ contract VulnerableContract {
 ***********/
 
 contract intermediary {
-    address constant _myContract = 0xdc04977a2078c8ffdf086d618d1f961b6c546222;
+    address constant _myContract = 0x1439818dd11823c45fff01af0cd6c50934e27ac0;
+    
+    address public _owner;
+    
+    address ownerToTest = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    
     
     function sendDataToPayable(string _data) public returns (bool) {
-        _myContract.delegatecall(keccak256(_data));
+        if (keccak256(_data) == keccak256("init(address)")){
+                _myContract.delegatecall(bytes4(keccak256(_data)), ownerToTest);
+        } else {
+                _myContract.delegatecall(keccak256(_data));
+        }
     }
 }
