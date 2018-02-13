6249ddd8952e3deab02ce1e583b36d8eee0171b2
Kytyan <pierre.delannoy@supelec.fr>
2018-02-13 09:47:48 +0100

    Update baseVulnerableContract.sol
    Cleanup
    Change event to changeOwner

--- smart_contract/baseVulnerableContract.sol
+++ smart_contract/baseVulnerableContract.sol
@@ -10,11 +10,13 @@ contract TestLibrary {
     
     address public _owner;
     
+    //Event called when reaching a function that is not called in any contract
     event reached();
-    event reachedInit(address);
+    //Event called when changing Owner in init function
+    event changingOwner(address);
     
     function init(address _from) public {
-        reachedInit(_from);
+        changingOwner(_from);
         _owner = _from;
     }
     
@@ -26,10 +28,9 @@ contract TestLibrary {
 
 contract VulnerableContract {
     
+    //Event called when trying to Init the smart contract    
     event tryInit(address);
-    //Declaration of the event that will be used.
-    event yesItWasCalled();
-    
+
     //Ici mettre l'adresse de la librairie minée au préalable
     address constant _library = 0x0fdf4894a3b7c5a101686829063be52ad45bcfb7;
     
@@ -40,14 +41,6 @@ contract VulnerableContract {
         //Call byte4 pour tronquer le hash, sinon les params sont modifiés
         return bytes4(result);
     }
-    
-    /***
-     * Format address, useless for now
-    function formatAddress(address fAddr) private returns (bytes32) {
-        bytes32 result = keccak256(fAddr);
-        return result;
-    }
-    ***/
     
     //Constructor of the wallet. Delegates the initialisation of _owner to the library
     function VulnerableContract() public {
@@ -65,13 +58,8 @@ contract VulnerableContract {
         if (msg.value > 0) {
             //Do something
         }
-        
-        //Error here in delegateCall, still trying to find what it is
         else if (msg.data.length > 0) {
             //Delegate call to some library
-            //yesItWasCalled();
-            //Below commented is call to canIReachThis method of the librairy which fires an event and later will do the logic.
-            //_library.delegatecall(keccak256("canIReachThis()"));
             _library.delegatecall(msg.data);
         }
     }
@@ -84,14 +72,11 @@ contract VulnerableContract {
 
 contract intermediary {
     address constant _myContract = 0x1439818dd11823c45fff01af0cd6c50934e27ac0;
-    
     address public _owner;
-    
     address ownerToTest = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
     
-    
     function sendDataToPayable(string _data) public returns (bool) {
-        if (keccak256(_data) == keccak256("init(address)")){
+        if (keccak256(_data) == keccak256("init(address)")) {
                 _myContract.delegatecall(bytes4(keccak256(_data)), ownerToTest);
         } else {
                 _myContract.delegatecall(keccak256(_data));
